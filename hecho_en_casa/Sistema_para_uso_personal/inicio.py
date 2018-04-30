b462c43d75baebd6807b25d9932bebbe7a2cc92f
Fjavier De Freitas <franjavikale@gmail.com>
2018-04-30 12:52:37 -0700

    modificaciones y pruebas en la db

--- hecho_en_casa/Sistema_para_uso_personal/inicio.py
+++ hecho_en_casa/Sistema_para_uso_personal/inicio.py
@@ -1,9 +1,15 @@
-import sqlite3
+from sqlite3 import connect
 
 #abrir la conexion
-conexion = sqlite3.connect('almacen.db')
+conexion = connect('almacen.db')
 
+#creando el cursor
+cursor = conexion.cursor()
 
+#consulta
+cursor.execute("SELECT cantidad, nombre, precios FROM productos WHERE cantidad > 0")
+datos = cursor.fetchall()
+print(datos)
 
 #cerrar la conexion
 conexion.close()
