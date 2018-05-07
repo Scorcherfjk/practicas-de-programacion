c7e658d1e7048a2ed68e80af3b3775698c3489fd
Fjavier De Freitas <franjavikale@gmail.com>
2018-05-07 03:27:15 -0700

    mejorando el filtrado anadiendo los tipos de medidas

--- hecho_en_casa/Sistema_para_uso_personal/inventario/operaciones_consulta.py
+++ hecho_en_casa/Sistema_para_uso_personal/inventario/operaciones_consulta.py
@@ -6,32 +6,32 @@ def res_long(datos):
 		print("no hay productos disponibles")
 	else:
 		for i in datos:
-			print("hay {} \t de {} \t de S/ {} cada uno, \t un total de S/ {:04.2f}".format(i[0],i[1],i[2],i[0]*i[2]))
+			print("hay {cantidad} \t de {nombre} \t de S/ {precio} cada {tipo_medida}, \t un total de S/ {total:04.2f}".format( cantidad=i[0], nombre=i[1], precio=i[2], total=i[0]*i[2], tipo_medida=i[3]))
 
 def res_short(datos):
 	if datos == []:
 			print("no hay productos disponibles")
 	else:
 		for n,i in enumerate(datos):
-			print("{}.- {} \t a S/ {} cada uno".format(n,i[0],i[1],))
+			print("{contador}.- {nombre} \t a S/ {precio} cada {tipo_medida}".format(contador=n, nombre=i[0], precio=i[1], tipo_medida=i[2] ))
 
 def prod_disp(cursor):
 	run("clear")
-	datos = cursor.execute("SELECT cantidad, nombre, precio FROM productos WHERE cantidad > 0").fetchall()
+	datos = cursor.execute("SELECT cantidad, nombre, precio, tipo_medida FROM productos WHERE cantidad > 0").fetchall()
 	res_long(datos)
 
 def prod_poca_exis(cursor):
 	run("clear")
-	datos = cursor.execute("SELECT cantidad, nombre, precio FROM productos WHERE cantidad BETWEEN 0.01 AND 3").fetchall()
+	datos = cursor.execute("SELECT cantidad, nombre, precio, tipo_medida FROM productos WHERE tipo_medida = 'un' AND cantidad BETWEEN 0.01 AND 3").fetchall()
 	res_long(datos)
 
 def prod_exis(cursor):
 	run("clear")
-	datos = cursor.execute("SELECT nombre, precio FROM productos WHERE cantidad = 0").fetchall()
+	datos = cursor.execute("SELECT nombre, precio, tipo_medida FROM productos WHERE cantidad = 0").fetchall()
 	res_short(datos)
 
 
 def prod_total(cursor):
 	run("clear")
-	datos = cursor.execute("SELECT nombre, precio FROM productos").fetchall()
+	datos = cursor.execute("SELECT nombre, precio, tipo_medida FROM productos").fetchall()
 	res_short(datos)
